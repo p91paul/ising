4dd7e26b17f13d6b28a114e5e1b881f4b2a6af9a
Paolo Inaudi <p91paul@gmail.com>
2014-11-19 00:25:22 +0100

    Add J matrix

--- CUDAIsing/ising.cu
+++ CUDAIsing/ising.cu
@@ -61,108 +61,101 @@ __global__ void initRNG(curandState * const rngStates,
     }
 }
 
-__global__  void fillMatrix(cudaPitchedPtr Sptr, curandState * const rngStates){
+__global__ void fillMatrix(int* S, int* Jx, int* Jy, int* Jz,
+        curandState * const rngStates) {
     unsigned int tid = blockIdx.x * SUM_BLOCK_SIZE + threadIdx.x;
     if (tid < L3) {
-        int* S = (int *) Sptr.ptr;
         //skipahead(100, &rngStates[tid]);
         S[tid] = randomSpin(rngStates, tid);
+        Jx[tid] = randomSpin(rngStates, tid);
+        Jy[tid] = randomSpin(rngStates, tid);
+        Jz[tid] = randomSpin(rngStates, tid);
     }
 }
 
-__device__ void tryInvert(int* S, dim3 index, float beta,
-        curandState * const rngStates) {
+template<char dir, int skip> __device__ int neigh(dim3 index) {
+    dim3 result = index;
+    if (dir == 'x')
+        result.x = (result.x + skip) % L;
+    if (dir == 'y')
+        result.y = (result.y + skip) % L;
+    if (dir == 'z')
+        result.z = (result.z + skip) % L;
+    return getTid(result);
+}
+
+__device__ int energy(int* S, int* Jx, int* Jy, int* Jz, dim3 index, int tid) {
+    int prevX = neigh<'x', -1>(index);
+    int prevY = neigh<'y', -1>(index);
+    int prevZ = neigh<'z', -1>(index);
+    int nEnergy = S[neigh<'x', 1>(index)] * Jx[tid] + S[prevX] * Jx[prevX]
+            + S[neigh<'y', 1>(index)] * Jy[tid] + S[prevY] * Jy[prevY]
+            + S[neigh<'z', 1>(index)] * Jz[tid] + S[prevZ] * Jz[prevZ];
+    return -S[tid] * nEnergy;
+}
+
+__device__ void tryInvert(int* S, int* Jx, int* Jy, int* Jz, dim3 index,
+        float beta, curandState * const rngStates) {
     if (index.x < L && index.y < L && index.z < L) {
-        //left
-        index.z = (index.z + 1) % L;
-        int neigh = S[getTid(index)];
-        //right
-        index.z = (index.z + L - 2) % L;
-        neigh += S[getTid(index)];
-        //up
-        index.z = (index.z + 1) % L;
-        index.y = (index.y + 1) % L;
-        neigh += S[getTid(index)];
-        //down
-        index.y = (index.y + L - 2) % L;
-        neigh += S[getTid(index)];
-        //forward
-        index.y = (index.y + 1) % L;
-        index.x = (index.x + 1) % L;
-        neigh += S[getTid(index)];
-        //backward
-        index.x = (index.x + L - 2) % L;
-        neigh += S[getTid(index)];
-        index.x = (index.x + 1) % L;
-        //energy
         unsigned int tid = getTid(index);
-        int dE = -2 * S[tid] * neigh;
+        int dE = 2 * energy(S, Jx, Jy, Jz, index, tid);
         if (dE < 0 || curand_uniform(&(rngStates[tid])) < __expf(-beta * dE))
-            S[tid] *= -1;
+            S[tid] = -S[tid];
     }
 }
 
-__global__ void generateNext(cudaPitchedPtr Sptr, float beta,
+__global__ void generateNext(int* S, int* Jx, int* Jy, int* Jz, float beta,
         curandState * const rngStates, int offset) {
-    int* S = (int *) Sptr.ptr;
     dim3 index = getIndex();
     index.z = offset + 2 * index.z + (index.x & 1) ^ (index.y & 1);
-    tryInvert(S, index, beta, rngStates);
+    tryInvert(S, Jx, Jy, Jz, index, beta, rngStates);
 }
-
 
 // Utility class used to avoid linker errors with extern
 // unsized shared memory arrays with templated type
 template<class T>
-struct SharedMemory
-{
-    __device__ inline operator       T *()
-    {
+struct SharedMemory {
+    __device__ inline operator T *() {
         extern __shared__ int __smem[];
-        return (T *)__smem;
+        return (T *) __smem;
     }
 
-    __device__ inline operator const T *() const
-    {
+    __device__ inline operator const T *() const {
         extern __shared__ int __smem[];
-        return (T *)__smem;
+        return (T *) __smem;
     }
 };
 
-
 /*
-    This version adds multiple elements per thread sequentially.  This reduces the overall
-    cost of the algorithm while keeping the work complexity O(n) and the step complexity O(log n).
-    (Brent's Theorem optimization)
+ This version adds multiple elements per thread sequentially.  This reduces the overall
+ cost of the algorithm while keeping the work complexity O(n) and the step complexity O(log n).
+ (Brent's Theorem optimization)
 
-    Note, this kernel needs a minimum of 64*sizeof(T) bytes of shared memory.
-    In other words if blockSize <= 32, allocate 64*sizeof(T) bytes.
-    If blockSize > 32, allocate blockSize*sizeof(T) bytes.
-*/
-template <class T, unsigned int blockSize, bool nIsPow2>
-__global__ void
-sum(T *g_idata, T *g_odata, unsigned int n)
-{
+ Note, this kernel needs a minimum of 64*sizeof(T) bytes of shared memory.
+ In other words if blockSize <= 32, allocate 64*sizeof(T) bytes.
+ If blockSize > 32, allocate blockSize*sizeof(T) bytes.
+ */
+template<class T, unsigned int blockSize, bool nIsPow2>
+__global__ void sum(T *g_idata, T *g_odata, unsigned int n) {
     T *sdata = SharedMemory<T>();
 
     // perform first level of reduction,
     // reading from global memory, writing to shared memory
     unsigned int tid = threadIdx.x;
-    unsigned int i = blockIdx.x*blockSize*2 + threadIdx.x;
-    unsigned int gridSize = blockSize*2*gridDim.x;
+    unsigned int i = blockIdx.x * blockSize * 2 + threadIdx.x;
+    unsigned int gridSize = blockSize * 2 * gridDim.x;
 
     T mySum = 0;
 
     // we reduce multiple elements per thread.  The number is determined by the
     // number of active thread blocks (via gridDim).  More blocks will result
     // in a larger gridSize and therefore fewer elements per thread
-    while (i < n)
-    {
+    while (i < n) {
         mySum += g_idata[i];
 
         // ensure we don't read out of bounds -- this is optimized away for powerOf2 sized arrays
         if (nIsPow2 || i + blockSize < n)
-            mySum += g_idata[i+blockSize];
+            mySum += g_idata[i + blockSize];
 
         i += gridSize;
     }
@@ -171,25 +164,21 @@ sum(T *g_idata, T *g_odata, unsigned int n)
     sdata[tid] = mySum;
     __syncthreads();
 
-
     // do reduction in shared mem
-    if ((blockSize >= 512) && (tid < 256))
-    {
+    if ((blockSize >= 512) && (tid < 256)) {
         sdata[tid] = mySum = mySum + sdata[tid + 256];
     }
 
     __syncthreads();
 
-    if ((blockSize >= 256) &&(tid < 128))
-    {
-            sdata[tid] = mySum = mySum + sdata[tid + 128];
+    if ((blockSize >= 256) && (tid < 128)) {
+        sdata[tid] = mySum = mySum + sdata[tid + 128];
     }
 
-     __syncthreads();
+    __syncthreads();
 
-    if ((blockSize >= 128) && (tid <  64))
-    {
-       sdata[tid] = mySum = mySum + sdata[tid +  64];
+    if ((blockSize >= 128) && (tid < 64)) {
+        sdata[tid] = mySum = mySum + sdata[tid + 64];
     }
 
     __syncthreads();
@@ -198,7 +187,7 @@ sum(T *g_idata, T *g_odata, unsigned int n)
     if ( tid < 32 )
     {
         // Fetch final intermediate sum from 2nd warp
-        if (blockSize >=  64) mySum += sdata[tid + 32];
+        if (blockSize >= 64) mySum += sdata[tid + 32];
         // Reduce final warp using shuffle
         for (int offset = warpSize/2; offset > 0; offset /= 2)
         {
@@ -207,61 +196,66 @@ sum(T *g_idata, T *g_odata, unsigned int n)
     }
 #else
     // fully unroll reduction within a single warp
-    if ((blockSize >=  64) && (tid < 32))
-    {
+    if ((blockSize >= 64) && (tid < 32)) {
         sdata[tid] = mySum = mySum + sdata[tid + 32];
     }
 
     __syncthreads();
 
-    if ((blockSize >=  32) && (tid < 16))
-    {
+    if ((blockSize >= 32) && (tid < 16)) {
         sdata[tid] = mySum = mySum + sdata[tid + 16];
     }
 
     __syncthreads();
 
-    if ((blockSize >=  16) && (tid <  8))
-    {
-        sdata[tid] = mySum = mySum + sdata[tid +  8];
+    if ((blockSize >= 16) && (tid < 8)) {
+        sdata[tid] = mySum = mySum + sdata[tid + 8];
     }
 
     __syncthreads();
 
-    if ((blockSize >=   8) && (tid <  4))
-    {
-        sdata[tid] = mySum = mySum + sdata[tid +  4];
+    if ((blockSize >= 8) && (tid < 4)) {
+        sdata[tid] = mySum = mySum + sdata[tid + 4];
     }
 
     __syncthreads();
 
-    if ((blockSize >=   4) && (tid <  2))
-    {
-        sdata[tid] = mySum = mySum + sdata[tid +  2];
+    if ((blockSize >= 4) && (tid < 2)) {
+        sdata[tid] = mySum = mySum + sdata[tid + 2];
     }
 
     __syncthreads();
 
-    if ((blockSize >=   2) && ( tid <  1))
-    {
-        sdata[tid] = mySum = mySum + sdata[tid +  1];
+    if ((blockSize >= 2) && (tid < 1)) {
+        sdata[tid] = mySum = mySum + sdata[tid + 1];
     }
 
     __syncthreads();
 #endif
 
     // write result for this block to global mem
-    if (tid == 0) g_odata[blockIdx.x] = mySum;
+    if (tid == 0)
+        g_odata[blockIdx.x] = mySum;
 }
 
-__global__ void print(cudaPitchedPtr Sptr) {
-    int* S = (int *) Sptr.ptr;
+__global__ void print(int* S) {
     for (int i = 0; i < L; ++i)
         for (int j = 0; j < L; ++j)
             for (int k = 0; k < L; ++k) {
                 dim3 index(i, j, k);
                 printf("%d,%d,%d: %d\n", i, j, k, S[getTid(index)]);
             }
+}
+
+__global__ void totalEnergy(int* S, int* Jx, int* Jy, int* Jz) {
+    int e = 0;
+    for (int i = 0; i < L; ++i)
+        for (int j = 0; j < L; ++j)
+            for (int k = 0; k < L; ++k) {
+                dim3 index(i, j, k);
+                e += energy(S, Jx, Jy, Jz, index, getTid(index));
+            }
+    printf("Total energy= %d\n", e);
 }
 
 class Configuration {
@@ -271,7 +265,19 @@ public:
         beta = 1 / T;
 
         cudaExtent extent = make_cudaExtent(L * sizeof(int), L, L);
-        CUDA_CHECK_RETURN(cudaMalloc3D(&ptr, extent));
+
+        cudaPitchedPtr ptrS;
+        cudaPitchedPtr ptrJx;
+        cudaPitchedPtr ptrJy;
+        cudaPitchedPtr ptrJz;
+        CUDA_CHECK_RETURN(cudaMalloc3D(&ptrS, extent));
+        CUDA_CHECK_RETURN(cudaMalloc3D(&ptrJx, extent));
+        CUDA_CHECK_RETURN(cudaMalloc3D(&ptrJy, extent));
+        CUDA_CHECK_RETURN(cudaMalloc3D(&ptrJz, extent));
+        this->ptrS = (int*) ptrS.ptr;
+        this->ptrJx = (int*) ptrJx.ptr;
+        this->ptrJy = (int*) ptrJy.ptr;
+        this->ptrJz = (int*) ptrJz.ptr;
 
         blocks = dim3(BLOCKS_X, BLOCKS_X, BLOCKS_X);
         threads.x = threads.y = BLOCK_SIZE;
@@ -280,10 +286,11 @@ public:
         CUDA_CHECK_RETURN(cudaMalloc(&rngStates, L3 * sizeof(curandState)));
 
         initRNG<<<SUM_NUM_BLOCKS * 2, SUM_BLOCK_SIZE>>>(rngStates, seed);
-        CUDA_CHECK_RETURN(cudaDeviceSynchronize());// Wait for the GPU launched work to complete
+        CUDA_CHECK_RETURN(cudaDeviceSynchronize()); // Wait for the GPU launched work to complete
         CUDA_CHECK_RETURN(cudaGetLastError());
-        fillMatrix<<<SUM_NUM_BLOCKS * 2, SUM_BLOCK_SIZE>>>(ptr, rngStates);
-        CUDA_CHECK_RETURN(cudaDeviceSynchronize());// Wait for the GPU launched work to complete
+        fillMatrix<<<SUM_NUM_BLOCKS * 2, SUM_BLOCK_SIZE>>>(this->ptrS,
+                this->ptrJx, this->ptrJy, this->ptrJz, rngStates);
+        CUDA_CHECK_RETURN(cudaDeviceSynchronize()); // Wait for the GPU launched work to complete
         CUDA_CHECK_RETURN(cudaGetLastError());
 
         CUDA_CHECK_RETURN(
@@ -294,22 +301,29 @@ public:
         CUDA_CHECK_RETURN(cudaGetLastError());
 
         CUDA_CHECK_RETURN(cudaFree(rngStates));
-        CUDA_CHECK_RETURN(cudaFree(ptr.ptr));
+        CUDA_CHECK_RETURN(cudaFree(ptrS));
+        CUDA_CHECK_RETURN(cudaFree(ptrJx));
+        CUDA_CHECK_RETURN(cudaFree(ptrJy));
+        CUDA_CHECK_RETURN(cudaFree(ptrJz));
+        CUDA_CHECK_RETURN(cudaFree(deviceSumPtr));
         CUDA_CHECK_RETURN(cudaDeviceReset());
     }
 
     void nextConfig() {
-        generateNext<<<blocks, threads>>>(ptr, beta, rngStates, 0);
-        CUDA_CHECK_RETURN(cudaDeviceSynchronize());// Wait for the GPU launched work to complete
+        generateNext<<<blocks, threads>>>(ptrS, ptrJx, ptrJy, ptrJz, beta,
+                rngStates, 0);
+        CUDA_CHECK_RETURN(cudaDeviceSynchronize()); // Wait for the GPU launched work to complete
         CUDA_CHECK_RETURN(cudaGetLastError());
-        generateNext<<<blocks, threads>>>(ptr, beta, rngStates, 1);
-        CUDA_CHECK_RETURN(cudaDeviceSynchronize());// Wait for the GPU launched work to complete
+        generateNext<<<blocks, threads>>>(ptrS, ptrJx, ptrJy, ptrJz, beta,
+                rngStates, 1);
+        CUDA_CHECK_RETURN(cudaDeviceSynchronize()); // Wait for the GPU launched work to complete
         CUDA_CHECK_RETURN(cudaGetLastError());
     }
 
     double getMagnet() {
-        sum<int, SUM_BLOCK_SIZE, true><<<SUM_NUM_BLOCKS, SUM_BLOCK_SIZE, SUM_BLOCK_SIZE * sizeof(int)>>>((int *)ptr.ptr, deviceSumPtr, L3);
-        CUDA_CHECK_RETURN(cudaDeviceSynchronize());// Wait for the GPU launched work to complete
+        sum<int, SUM_BLOCK_SIZE, true> <<<SUM_NUM_BLOCKS, SUM_BLOCK_SIZE,
+                SUM_BLOCK_SIZE * sizeof(int)>>>((int *) ptrS, deviceSumPtr, L3);
+        CUDA_CHECK_RETURN(cudaDeviceSynchronize()); // Wait for the GPU launched work to complete
         CUDA_CHECK_RETURN(cudaGetLastError());
         CUDA_CHECK_RETURN(
                 cudaMemcpy(hostSumPtr, deviceSumPtr,
@@ -319,13 +333,20 @@ public:
         for (int i = 0; i < SUM_NUM_BLOCKS; ++i) {
             result += hostSumPtr[i];
         }
-        return result/ (T+1);
+        return result;
     }
 
     void printMatrix(int i) {
         cout << "iteration " << i << endl;
-        print<<<1, 1>>>(ptr);
-        CUDA_CHECK_RETURN(cudaDeviceSynchronize());// Wait for the GPU launched work to complete
+        print<<<1, 1>>>(ptrS);
+        CUDA_CHECK_RETURN(cudaDeviceSynchronize()); // Wait for the GPU launched work to complete
+        CUDA_CHECK_RETURN(cudaGetLastError());
+    }
+
+    void printEnergy(int i) {
+        cout << "iteration " << i << ": ";
+        totalEnergy<<<1, 1>>>(ptrS, ptrJx, ptrJy, ptrJz);
+        CUDA_CHECK_RETURN(cudaDeviceSynchronize()); // Wait for the GPU launched work to complete
         CUDA_CHECK_RETURN(cudaGetLastError());
     }
 
@@ -339,14 +360,17 @@ private:
     curandState *rngStates;
     dim3 blocks;
     dim3 threads;
-    cudaPitchedPtr ptr;
+    int* ptrS;
+    int* ptrJx;
+    int* ptrJy;
+    int* ptrJz;
     int* deviceSumPtr;
     int hostSumPtr[SUM_NUM_BLOCKS];
 };
 
 int main(int argc, char** argv) {
     double T = 0;
-    unsigned int N = 2;
+    unsigned int N = 82;
     if (argc >= 2)
         T = atoi(argv[1]);
     if (argc >= 3)
@@ -356,7 +380,10 @@ int main(int argc, char** argv) {
     for (int i = 0; i < N; i++) {
         S.nextConfig();
         //S.printMatrix(i);
-        sum += S.getMagnet();
+        double magnet = S.getMagnet();
+        sum += magnet;
+        S.printEnergy(i);
+        cout << magnet << endl;
     }
     cout << sum / N << endl;
 }
